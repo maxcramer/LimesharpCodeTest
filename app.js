1cb9928b83d9d66eb5668b8c6c6a9ccb22334e58
Max Cramer <maximiliancramer@yahoo.com>
2019-10-30 15:29:47 +0000

    second task complete

--- app.js
+++ app.js
@@ -7,3 +7,51 @@ function repeat(array) {
 }
 
 repeat([1, 2, 3]);
+
+function reformat(str) {
+  var vowels = 'aeiou';
+  var strArr = str.toLowerCase().split('');
+  var newArr = strArr.filter(function(letter) {
+    if(vowels.indexOf(letter) == -1) {
+      return letter;
+    }
+  });
+
+  var string = '';
+  newArr.forEach(function(letter) {
+    string += letter;
+  });
+  return string;
+}
+
+reformat('liMeSHArp DeveLoper TEST');
+
+
+// function reformat() {
+//   var string = 'liMeSHArp DeveLoper TEST';
+//   var lc = string.toLocaleLowerCase();
+//   var vowels = ['a', 'e', 'i', 'o', 'u'];
+//   return lc.split('').filter(function(lc) {
+//     return vowels.indexOf(lc.toLowerCase() == -1);
+//   }).join('');
+//
+// }
+//
+// reformat();
+
+
+
+// function reformat(text) {
+//   var result = '';
+//   for(i = 0; i < text.length; i++) {
+//
+//     var c = text[i]
+//
+//     if('aeiou'.match(c.toLowerCase()) === null) {
+//       result += c;
+//     }
+//   }
+//   console.log(c);
+// }
+//
+// reformat();
